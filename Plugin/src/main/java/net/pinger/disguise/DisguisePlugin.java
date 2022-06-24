892f4977f5c59f473d173b1edd6650a695a1f2fc
Dimitrije Mijailovic <pinger.business@gmail.com>
2022-06-24 13:09:01 +0200

    Standard JavaPlugin implementation

---
+++ Plugin/src/main/java/net/pinger/disguise/DisguisePlugin.java
@@ -0,0 +1,16 @@
+package net.pinger.disguise;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DisguisePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
