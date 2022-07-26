34ea6edf5021528131216d0db45dc650c7740ba9
Dimitrije Mijailovic <pinger.business@gmail.com>
2022-07-26 19:51:28 +0200

    Remove unused import from test class

--- API/src/test/java/net/pinger/disguise/server/MinecraftServerTest.java
+++ API/src/test/java/net/pinger/disguise/server/MinecraftServerTest.java
@@ -2,7 +2,6 @@ package net.pinger.disguise.server;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MinecraftServerTest {
