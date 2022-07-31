54baa42e09969936cb0ff9c4e993c076df24d7bd
Dimitrije Mijailovic <pinger.business@gmail.com>
2022-07-31 14:58:28 +0200

    Remove javadoc from PropertyContext

--- API/src/main/java/net/pinger/disguise/context/PropertyContext.java
+++ API/src/main/java/net/pinger/disguise/context/PropertyContext.java
@@ -8,9 +8,6 @@ public class PropertyContext {
     /**
      * Creates a new property from the given skin details
      * which then can be applied to a player that is online on the server.
-     * <p>
-     * Note that after 1.7 Minecraft version the Property package
-     * has been changed from {@link net.minecraft.util.com.mojang.authlib.properties.Property} to {@link com.mojang.authlib.properties.Property}.
      *
      * @param skin the skin
      * @return a new property object
